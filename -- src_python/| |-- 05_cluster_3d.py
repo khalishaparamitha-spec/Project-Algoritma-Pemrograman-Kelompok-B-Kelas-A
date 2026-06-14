d7ff72279734bb73e8110aa88bbcda867e0a3422
nonikf <nonik.zahro@mhs.unsoed.ac.id>
2026-06-14 20:15:56 +0700

    Update | |-- 05_cluster_3d.py

--- -- src_python/| |-- 05_cluster_3d.py
+++ -- src_python/| |-- 05_cluster_3d.py
@@ -1,16 +1,20 @@
 import math
 
+# input + validasi
+try:
+    x1 = float(input("Masukkan x1: "))
+    x2 = float(input("Masukkan x2: "))
+    x3 = float(input("Masukkan x3: "))
+except:
+    print("input tidak valid")
+    exit()
+
+U = (x1, x2, x3)
+
 # titik pusat cluster
 A = (2, 1, 3)
 B = (1, -4, 6)
 C = (-2, 3, -2)
-
-# input titik U
-x1 = float(input("Masukkan x1: "))
-x2 = float(input("Masukkan x2: "))
-x3 = float(input("Masukkan x3: "))
-
-U = (x1, x2, x3)
 
 # fungsi jarak
 def jarak(P, Q):
@@ -26,10 +30,23 @@ print("Jarak ke A:", dA)
 print("Jarak ke B:", dB)
 print("Jarak ke C:", dC)
 
-# menentukan cluster
-if dA <= dB and dA <= dC:
+# kondisi
+if dA == dB or dA == dC or dB == dC:
+    if dA == dB and dA == dC:
+        print("Jarak sama: A = B = C")
+    elif dA == dB:
+        print("Jarak sama: A = B")
+    elif dA == dC:
+        print("Jarak sama: A = C")
+    elif dB == dC:
+        print("Jarak sama: B = C")
+
+elif dA < dB and dA < dC:
     print("Titik U masuk cluster A")
-elif dB <= dA and dB <= dC:
+
+elif dB < dA and dB < dC:
     print("Titik U masuk cluster B")
-else:
+
+elif dC < dA and dC < dB:
     print("Titik U masuk cluster C")
+ C")
