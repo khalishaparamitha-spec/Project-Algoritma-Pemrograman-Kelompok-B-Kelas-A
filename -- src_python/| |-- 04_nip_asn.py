7985513aae3e30793c53bbde030e810f9a7b2a8f
wafiawaliya <wafi.awaliya@mhs.unsoed.ac.id>
2026-06-16 17:57:09 +0700

    Update | |-- 04_nip_asn.py

--- -- src_python/| |-- 04_nip_asn.py
+++ -- src_python/| |-- 04_nip_asn.py
@@ -1,43 +1,64 @@
-# Menerima input NIP dari pengguna
+#Menerima input NIP dari pengguna
 nip = input("Masukkan NIP ASN:")
-# Ekstrak tahun, bulan, dan tanggal menggunakan slicing (semuanya bertipe string)
+
+#Ekstrak tahun, bulan, dan tanggal dengan menggunakan slicing
 tahun = nip[0:4]
 bulan = nip[4:6]
 tanggal = nip[6:8]
 
-#Percabangan untuk membatasi tanggal
-if int(tanggal) >= 1 and int(tanggal) <= 31:
-  tanggal_lahir = tanggal
-else:
-    tanggal_lahir = "Tanggal lahir error"
+#Mengubah ke integer 
+tanggal_angka = int(tanggal)
+tahun_angka = int(tahun)
 
-#Percabangan untuk menentukan nama bulan menggunakan perbandingan string
+#Menggunakan struktur percabangan if-else bertingkat untuk menentukan nama bulan
 if bulan == "01":
     nama_bulan = "Januari"
+    hari_maks = 31
 elif bulan == "02":
     nama_bulan = "Februari"
+    if (tanggal_angka % 4 == 0 and tahun_angka % 100 != 0 ) or (tahun_angka % 400 == 0):
+      hari_maks = 29
+    else:
+      hari_maks = 28
 elif bulan == "03":
     nama_bulan = "Maret"
+    hari_maks = 31
 elif bulan == "04":
     nama_bulan = "April"
+    hari_maks = 30
 elif bulan == "05":
     nama_bulan = "Mei"
+    hari_maks = 31
 elif bulan == "06":
     nama_bulan = "Juni"
+    hari_maks = 30
 elif bulan == "07":
     nama_bulan = "Juli"
+    hari_maks = 31
 elif bulan == "08":
     nama_bulan = "Agustus"
+    hari_maks = 31
 elif bulan == "09":
     nama_bulan = "September"
+    hari_maks = 30
 elif bulan == "10":
     nama_bulan = "Oktober"
+    hari_maks = 31
 elif bulan == "11":
     nama_bulan = "November"
+    hari_maks = 30
 elif bulan == "12":
     nama_bulan = "Desember"
+    hari_maks = 31
 else:
     nama_bulan = "Bulan tidak valid"
+    hari_maks = 0
+  
+# Validasi apakah tanggal berada di rentang yang sah sesuai bulannya
+if tanggal_angka < 1 or tanggal_angka > hari_maks:
+    tanggal_status = "Tanggal tidak valid"
+else:
+    tanggal_status = tanggal
 
-# Tampilkan hasil akhir ke layar
-print(f"Tanggal lahir ASN: {tanggal_lahir} {nama_bulan} {tahun}")
+#Menampilkan hasil akhir
+print(f"Tanggal lahir ASN: {tanggal_status} {nama_bulan} {tahun}")
