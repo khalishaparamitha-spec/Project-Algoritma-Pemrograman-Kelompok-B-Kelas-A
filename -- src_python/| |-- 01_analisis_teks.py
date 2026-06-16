dd4145ad5e6bafaa6a8a21e9a8668c36fc202586
herlinasari-byte <herlina.sari@mhs.unsoed.ac.id>
2026-06-16 17:48:22 +0700

    -- 01_analisis_teks.py

--- -- src_python/| |-- 01_analisis_teks.py
+++ -- src_python/| |-- 01_analisis_teks.py
@@ -1,1 +1,17 @@
-
+#Menghitung jumlah kata dan jumlah kalimat
+#Memisahkan teks berdasarkan spasi
+def hitung_kata(teks):
+   kata = teks.split()
+   return len(kata)
+#Memisahkan teks berdasarkan tanda titik
+def hitung_kalimat(teks):
+   bagian = teks.split(".")
+   jumlah = len([b for b in bagian if b.strip() != ""]) #Menyaring bagian kosong
+   return jumlah
+#Input dari pengguna
+teks = input("Masukkan teks: ")
+#Proses perhitungan
+jumlah_kata = hitung_kata(teks)
+jumlah_kalimat = hitung_kalimat(teks)
+#Output hasil
+print(f"Teks tersebut memuat {jumlah_kalimat} kalimat dan {jumlah_kata} kata.")
