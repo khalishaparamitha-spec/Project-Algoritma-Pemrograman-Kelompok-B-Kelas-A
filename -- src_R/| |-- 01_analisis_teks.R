5a91d5d9b8a149971d065aa517e75752129e3a0d
herlinasari-byte <herlina.sari@mhs.unsoed.ac.id>
2026-06-16 17:50:34 +0700

    | |-- 01_analisis_teks.R

--- -- src_R/| |-- 01_analisis_teks.R
+++ -- src_R/| |-- 01_analisis_teks.R
@@ -1,1 +1,23 @@
+#Program 1: Analisis Teks - Menghitung Jumlah Kata dan Jumlah Kalimat
+hitung_kata <- function(teks) {
+  #Memisahkan teks berdasarkan spasi lalu menghitung elemen
+  kata <- unlist(strsplit(trimws(teks), "\\s+"))
+  return(length(kata))
+}
+hitung_kalimat <- function(teks) {
+  
+  #Memisahkan teks berdasarkan tanda titik, lalu hitung bagian tidak kosong
+  bagian <- strsplit(teks, "\\.")[[1]]
+  jumlah <- length(bagian[trimws(bagian) != ""])
+  return(jumlah)
+}
 
+#Input dari pengguna
+teks <- readline(prompt = "Masukkan teks: ")
+
+#Proses perhitungan
+jumlah_kata <- hitung_kata(teks)
+jumlah_kalimat <- hitung_kalimat(teks)
+
+#Output hasil
+print(paste0("Teks tersebut memuat ", jumlah_kalimat, " kalimat dan ", jumlah_kata, " kata."))
